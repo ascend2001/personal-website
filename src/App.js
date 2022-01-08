bbb44c0739dab223749721817dec2897112175d1
ascend2001 <avnishsengupta1@g.ucla.edu>
2022-01-08 08:49:39 -0800

    Added responsiveness to Experience and Extracurriculars

--- src/App.js
+++ src/App.js
@@ -14,11 +14,26 @@ import dsuLogo from './assets/dsuLogo.png';
 import uclaLogo from './assets/uclaLogo.png';
 import DMLogo from './assets/datamatchLogo.png';
 import IEEELogo from './assets/IEEELogo.png';
-import transparent from './assets/transparent.png';
+// import transparent from './assets/transparent.png';
+
+const apprenticeWork = ['Increased sales tracking efficiency by over 70% by automating tracking on commission dashboard using Excel and Zapier.',
+  'Led a comparative UI/UX analysis between the companys old website and 10+ other Talent Acquisition Companies with a team of 4 people, consistently communicating and incorporating the decisions made by stakeholders and designers.',
+  'Boosted exposure over 50% by developing a revamped website for the company using React, HTML, CSS, and Node'];
+
+const hussleWork = ['Worked on the development of the beta version of a cross-platform mobile app on a team of 10 developers for clients to offer and buy amateur services.',
+  'Augmented a settings interface onto the user profile using React Native, Tab and Stack React Navigation, and Expo APIs.',
+  'Led UI/UX analysis on the current interface and identified 10+ pain points with optimal solutions on a team of 5 people.'];
+
+const LAWork = ['Tutored and facilitated instruction to 200+ students through weekly lectures, workshops, review sessions, and office hours.',
+  'Developed content of class to maximize student learning through weekly content meetings with the instructor to design 20+ assignments.',
+  'Increased student engagement by over 45% by incorporating feedback from students and peer observations to our lecture'];
+
+const IDEAHacksWork = ['Spearheaded correspondence and detail-oriented negotiations with 30+ sponsors for Southern California’s largest hackathon.',
+  'Secured 20% of the sponsorships on the team including Chipotle offering cash prizes of eight $50 gift cards and 2 meals of $100'];
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col">
       <header className="initial-wrapper">
         <div className="absolute">
           <img src={stars} alt="" width="1392.3" height="695" className="blur-sm" />
@@ -44,13 +59,13 @@ function App() {
         </div>
       </header>
       <section className="flex flex-row second-wrapper text-white">
-        <div className=" flex flex-col mt-6 ml-12 basis-2/3">
+        <div className=" flex flex-col mt-6 ml-12 lg:basis-2/3">
           <div className="text-6xl mt-8 ml-4">Experiences</div>
-          <ExpCard imageName={hussleLogo} company="Hussle" role="Frontend Engineering Intern" timeline="September 2021 - December 2021" location="Los Angeles, CA" />
-          <ExpCard imageName={apprenticeLogo} company="Apprentice" role="Software Engineering Intern" timeline="August 2021 - October 2021" location="Los Angeles, CA" />
+          <ExpCard imageName={hussleLogo} company="Hussle" work={hussleWork} role="Frontend Engineering Intern" timeline="September 2021 - December 2021" location="Los Angeles, CA" />
+          <ExpCard imageName={apprenticeLogo} work={apprenticeWork} company="Apprentice" role="Software Engineering Intern" timeline="August 2021 - October 2021" location="Los Angeles, CA" />
 
         </div>
-        <img src={leaningMan} alt="" width="203.5" height="659" className="place-items-center basis-1/3 my-10" />
+        <img src={leaningMan} alt="" width="203.5" height="659" className="leaning-man-img place-items-center lg:basis-1/3 sm:hidden lg:flex my-48" />
       </section>
       <section className="flex flex-row third-wrapper text-white">
         <div className="lg:basis-2/3 sm:basis-3/3 flex flex-col">
@@ -58,26 +73,20 @@ function App() {
           <div className="flex flex-col mt-6 ml-12">
             <ExpCard imageName={bpLogo} company="LA Blueprint" role="Incoming Full-Stack Developer" timeline="January 2022" location="Los Angeles, CA" />
             <ExpCard imageName={dsuLogo} company="Data Science Union" role="Incoming Data Analyst" timeline="January 2022" location="Los Angeles, CA" />
-            <ExpCard imageName={uclaLogo} company="Undergraduate Learning Assistant Program at UCLA" role="Math 31AL Learning Assistant" timeline="September 2021 - December 2021" location="Los Angeles, CA" />
-            <ExpCard imageName={DMLogo} company="Datamatch at UCLA" role="Vice President of Social Affairs" timeline="January 2021 - Present" location="Los Angeles, CA" />
-            <ExpCard imageName={IEEELogo} company="IEEE Student Branch at UCLA" role="Company Outreach | UCLA IDEA Hacks 2021" timeline="May 2020 - January 2021" location="Los Angeles, CA" />
-            <ExpCard imageName={transparent} role="IEEE Open Project Space (OPS)" timeline="September 2019 - May2020" location="Los Angeles, CA" />
+            <ExpCard imageName={uclaLogo} work={LAWork} company="Undergraduate Learning Assistant Program at UCLA" role="Math 31AL Learning Assistant" timeline="September 2021 - December 2021" location="Los Angeles, CA" />
+            <ExpCard imageName={DMLogo} company="Datamatch at UCLA" role="Vice President of Social Affairs" timeline="June 2021 - Present" location="Los Angeles, CA" />
+            <ExpCard imageName={IEEELogo} work={IDEAHacksWork} company="IEEE at UCLA" role="Company Outreach | UCLA IDEA Hacks 2021" timeline="May 2020 - January 2021" location="Los Angeles, CA" />
           </div>
         </div>
-        <img src={SkillsChest} alt="" width="536" height="798" className="place-items-center lg:basis-1/3 sm:hidden lg:flex" />
+        <div className="flex place-items-center lg:basis-1/3">
+          <img src={SkillsChest} alt="" width="536" height="798" className="skillsChest-img" />
+        </div>
       </section>
       <section className="flex flex-row fourth-wrapper">
-        <div className="flex flex-col basis-2/3 text-3xl place-content-center">
-          <div>The quick brown fox jumped over the old lazy dog.</div>
-          <div>The quick brown fox jumped over the old lazy dog.</div>
-          <div>The quick brown fox jumped over the old lazy dog.</div>
-          <div>The quick brown fox jumped over the old lazy dog.</div>
-          <div>The quick brown fox jumped over the old lazy dog.</div>
-          <div>lorem ipsum dolsum lorem ipsum dolsum</div>
-        </div>
+        <div className="flex flex-col basis-2/3 text-3xl place-content-center" />
         <div className="flex flex-col justify-around relative">
           <img src={askerSpeech} alt="" width="280" height="144.25" />
-          <div className="speech-bubble-wrapper absolute top-20 left-8 text-lg">
+          <div className="speech-bubble-wrapper absolute top-16 left-12 text-lg">
             Which github branch are you on?
           </div>
           <img src={laptop} alt="" width="440.5" height="239.5" className="basis-1/3" />
--- src/ExpCard.js
+++ src/ExpCard.js
@@ -4,13 +4,13 @@ import propTypes from 'prop-types';
 import bullet from './assets/bullet.svg';
 
 function ExpCard({
-  imageName, company, role, timeline, location,
+  imageName, company, role, timeline, location, work,
 }) {
   return (
     <div className="flex flex-col my-6">
       <div className="flex flex-row place-items-center">
         <img src={imageName} alt="" width="75" height="75" />
-        <div className="flex flex-col ml-6 justify-end">
+        <div className="flex flex-col ml-6 justify-end grow">
           <div className="experience-card-company-name text-4xl">{company}</div>
           <div className="experience-card-company-role text-2xl">{role}</div>
         </div>
@@ -20,29 +20,14 @@ function ExpCard({
         </div>
       </div>
       <div className="flex flex-col mt-7">
-        <div className="flex flex-row ml-12 items-start my-1.5">
-          <img src={bullet} alt="" height="10" width="10" className="mt-2" />
-          <div className="ml-2 experience-bullet-text text-lg">
-            Increased sales tracking efficiency by over 70% by automating tracking
-            on commission dashboard using Excel and Zapier.
+        { work.map((element) => (
+          <div className="flex flex-row ml-12 items-start my-1.5">
+            <img src={bullet} alt="" height="10" width="10" className="mt-2" />
+            <div className="ml-2 experience-bullet-text text-lg">
+              {element}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-row ml-12 items-start my-1.5">
-          <img src={bullet} alt="" height="10" width="10" className="mt-2" />
-          <div className="ml-2 experience-bullet-text text-lg">
-            Led a comparative UI/UX analysis between the company’s old website
-            and 10+ other Talent Acquisition Companies with a team of 4 people,
-            consistently communicating and incorporating the decisions made by
-            stakeholders and designers.
-          </div>
-        </div>
-        <div className="flex flex-row ml-12 items-start my-1.5">
-          <img src={bullet} alt="" height="10" width="10" className="mt-2" />
-          <div className="ml-2 experience-bullet-text text-lg">
-            Boosted exposure over 50% by developing a revamped website for the
-            company using React, HTML, CSS, and Node
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
@@ -51,6 +36,7 @@ function ExpCard({
 ExpCard.defaultProps = {
   location: '',
   company: '',
+  work: [],
 };
 
 ExpCard.propTypes = {
@@ -59,6 +45,7 @@ ExpCard.propTypes = {
   role: propTypes.string.isRequired,
   timeline: propTypes.string.isRequired,
   location: propTypes.string,
+  work: propTypes.arrayOf(propTypes.string),
 };
 
 export default ExpCard;
